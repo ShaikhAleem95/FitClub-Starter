2342ba71be8f790168074b304adbc06a9b041c64
ShaikhAleem <ShaikhAleem95@gmail.com>
2023-12-25 14:20:01 +0530

    resons

--- src/components/Reasons/Reasons.jsx
+++ src/components/Reasons/Reasons.jsx
@@ -4,8 +4,9 @@ import image1 from "../../assets/image1.png";
 import image2 from "../../assets/image2.png";
 import image3 from "../../assets/image3.png";
 import image4 from "../../assets/image4.png";
-import nb from "../../assets/adidas.png";
- import nike from "../../assets/nike.png";
+import nb from "../../assets/nb.png";
+import adidas from "../../assets/adidas.png";
+import nike from "../../assets/nike.png";
 import tick from "../../assets/tick.png";
 
 
@@ -23,7 +24,7 @@ const Reasons = () => {
                 <img src={image3} alt="" />
                 <img src={image4} alt="" />
         </div>
-        <div className="right-r">
+        <div className='right-r'>
          <span>some reasons</span>
 
          <div>
@@ -31,33 +32,40 @@ const Reasons = () => {
           <span> Choose Us?</span>
           </div>
           
-        <div ClassName="details-r">
+        <div className='details-r'>
                 <div>
-                          <img src={tick} alt=""></img> 
+                          <img src={tick} alt="" /> 
                           <span>Over 140+  EXPERT COACHS</span>
                 </div>
                  <div>
-                      <img src={tick} alt=""></img> 
+                      <img src={tick} alt="" /> 
                       <span>TRAIN SMARTER AND FASTER THEN BE</span>
                 </div>
                  <div>
-                  <img src={tick} alt=""></img> 
+                  <img src={tick} alt="" /> 
                   <span>1 FREE PROGRAM FOR NEW MEMBER</span>
                  </div>
 
                  <div>
-                    <img src={tick} alt=""></img> 
+                    <img src={tick} alt="" /> 
                      <span>RELIABLE PARTNERS</span>
                  </div>
          </div>
           
-         <span style={{
+         <span
+          style={{
           color: "var(--gray)",
-          fontWeight:"normal",
+          fontWeight: "normal",
         }}
         >
-        OUR PARTNERS</span>
-    
+        OUR PARTNERS
+      </span>
+        <div className="partners">
+          <img src={nb} alt="" />
+          <img src={adidas} alt="" />
+          <img src={nike} alt="" />
+        </div>
+
     </div>
        
     </div>

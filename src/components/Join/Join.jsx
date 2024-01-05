1fd4312ebc031f8baa43d6695892e6c99243bf1f
ShaikhAleem <ShaikhAleem95@gmail.com>
2024-01-05 14:35:25 +0530

    join

--- src/components/Join/Join.jsx
+++ src/components/Join/Join.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {useRef} from "react";
 import "./Join.css";
+import emailjs from '@emailjs/browser';
+ 
 
 const Join = () => {
+
+  const form=useRef()
+
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_edprnge', 'template_jdbzsyd', form.current, 'qabP_uMHOC2J7Yu-C')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };  
+
+
   return (
     <div className="Join" id="join-us">
       <div className="left-j">
         <div>
-          <span>READY TO </span>
+          <span className="stroke-text">READY TO </span>
           <span>LEVEL UP</span>
         </div>
 
@@ -16,9 +34,13 @@ const Join = () => {
           </div>
       </div>
 
-      <div className="right-j">
+            <div className="right-j">
+              <form  ref={form} action="" className="email-container" onsubmit={sendEmail}>
+                <input type="text" class="user_email" placeholder="Enter Your E-mail Address" />
+               <button className="btn btn-j">Join Now</button>
+              </form> 
 
-      </div>
+            </div>
     </div>
   );
 };
